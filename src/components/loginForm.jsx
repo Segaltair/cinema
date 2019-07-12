9e6f06ea24fe04163844134aea114d81b325912f
segaltair <ssurakin@gmail.com>
2019-07-12 12:21:15 +0300

    add login form validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,17 +3,12 @@ import Input from "./common/input";
 
 class LoginForm extends Component {
     state = {
-        account: {username: "", password: ""}
-    };
-
-    handleChange = ({currentTarget: input}) => {
-        const account = {...this.state.account};
-        account[input.name] = input.value;
-        this.setState({account});
+        account: {username: "", password: ""},
+        errors: {}
     };
 
     render() {
-        const {account} = this.state;
+        const {account, errors} = this.state;
 
         return (
             <div>
@@ -23,20 +18,66 @@ class LoginForm extends Component {
                         name="username"
                         label="Username"
                         value={account.username}
-                        onChange={this.handleChange}/>
+                        onChange={this.handleChange}
+                        error={errors.username}
+                    />
                     <Input
                         name="password"
                         label="Password"
                         value={account.password}
-                        onChange={this.handleChange}/>
+                        onChange={this.handleChange}
+                        error={errors.password}
+                    />
                     <button className="btn btn-primary">Login</button>
                 </form>
             </div>
         );
     }
 
+    validate = () => {
+        const errors = {};
+
+        const {account} = this.state;
+        if (account.username.trim() === "")
+            errors.username = "Username is required";
+
+        if (account.password.trim() === "")
+            errors.password = "Password is required";
+
+        return Object.keys(errors).length === 0 ? null : errors;
+    };
+
+    validateProperty = ({name, value}) => {
+        if (name === "username") {
+            if (value.trim() === "") return "Username is required";
+        }
+
+        if (name === "password") {
+            if (value.trim() === "") return "Password is required";
+        }
+    };
+
+    handleChange = ({currentTarget: input}) => {
+        const errors = {...this.state.errors};
+        const errorMessage = this.validateProperty(input);
+        if (errorMessage)
+            errors[input.name] = errorMessage;
+        else
+            delete errors[input.name];
+
+
+        const account = {...this.state.account};
+        account[input.name] = input.value;
+        this.setState({account, errors});
+    };
+
     handleSubmit = e => {
         e.preventDefault();
+
+        const errors = this.validate();
+        this.setState({errors: errors || {}});
+
+        if (errors) return;
 
         console.log("submit")
     }
