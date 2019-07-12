e1766c72b507ba586d1bee36e97e258d5eb58e6b
segaltair <ssurakin@gmail.com>
2019-07-12 11:01:14 +0300

    handle username

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,14 +1,29 @@
 import React, {Component} from "react";
 
 class LoginForm extends Component {
+    state = {
+        account: {username: "", password: ""}
+    };
+
+    handleChange = e => {
+        const account = {...this.state.account};
+        account.username = e.currentTarget.value;
+        this.setState({account});
+    };
+
     render() {
         return (
             <div>
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input id="username" type="text" className="form-control"/>
+                        <input value={this.state.account.username}
+                               onChange={this.handleChange}
+                               autoFocus
+                               ref={this.username}
+                               id="username"
+                               type="text" className="form-control"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
@@ -18,6 +33,13 @@ class LoginForm extends Component {
                 </form>
             </div>
         );
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const username = this.username.current.value;
+        console.log(username)
     }
 }
 
